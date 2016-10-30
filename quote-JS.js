804dd5375ba5efa6d3f22fe36a7162bfe8de8692
Ty <tyler.peterson@utah.edu>
2016-10-30 16:48:03 -0600

    Move curley quotes and add line break after quote

--- quote-JS.js
+++ quote-JS.js
@@ -10,15 +10,15 @@ $(document).ready(function(){
 
       //quotes array    
       var quotes = [
-        "It turns out Jesus is a squirrel. A big fat meaty one. And I shot and ate that son of a bitch. - John Fitzgerald",
-        "My heart bleeds. But revenge is in the creator's hands. - Hikuc",
-        "I ain't afraid to die anymore. I'd done it already. - Hugh Glass",
-        "I guess... I can't help thinking about whether we did the right... - Bridger",
-        "I can't remember my wife's face. I worry I won't recognize her when I get back home. - Andrew Henry",
-        "You better take that last breath of yours now! - John Fitzgerald",
-        "You all have stolen everything from us. Everything! The land. The animals. - Elk Dog",
-        "God giveth, God taketh away. - John Fitzgerald",
-        "At least he heard me. - Hawk"        
+        "&ldquo;It turns out Jesus is a squirrel. A big fat meaty one. And I shot and ate that son of a bitch.&rdquo;<br/> - John Fitzgerald",
+        "&ldquo;My heart bleeds. But revenge is in the creator's hands.&rdquo;<br/> - Hikuc",
+        "&ldquo;I ain't afraid to die anymore. I'd done it already.&rdquo;<br/> - Hugh Glass",
+        "&ldquo;I guess... I can't help thinking about whether we did the right...&rdquo;<br/> - Bridger",
+        "&ldquo;I can't remember my wife's face. I worry I won't recognize her when I get back home.&rdquo;<br/> - Andrew Henry",
+        "&ldquo;You better take that last breath of yours now!&rdquo;<br/> - John Fitzgerald",
+        "&ldquo;You all have stolen everything from us. Everything! The land. The animals.&rdquo;<br/> - Elk Dog",
+        "&ldquo;God giveth, God taketh away.&rdquo;<br/> - John Fitzgerald",
+        "&ldquo;At least he heard me.&rdquo;<br/> - Hawk"        
       ];
       
       // get random id from array  
